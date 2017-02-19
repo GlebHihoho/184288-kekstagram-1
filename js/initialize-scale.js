7fefbf9e655feb51ef04620d26efd16325c8f8e9
Gleb Motuz <hihoho@mail.ru>
2017-02-19 19:52:33 +0300

    module6-task2
    Полностью переписал модули с использованием ф-й обратного вызова.

--- js/initialize-scale.js
+++ js/initialize-scale.js
@@ -1,57 +1,33 @@
 'use strict';
 
 (function() {
+  var scaleElement = document.querySelector('.upload-resize-controls');
+  var pictureElement = document.querySelector('.filter-image-preview');
 
-  var initializeScale = function(element, step, defaultValue) {
+  var adjustScale = function(scale) {
+    pictureElement.style.transform = 'scale(' + scale / 100 + ')';
+  };
 
-    var scaleField = element.querySelector('.upload-resize-controls-value');            // поле отображающее текущее значение масштаба
-    var upScaleField = element.querySelector('.upload-resize-controls-button-inc');     // кнопка отвечающая за увеличение масштаба изображения
-    var downScaleField = element.querySelector('.upload-resize-controls-button-dec');   // кнопка отвечающая за уменьшение масштаба изображения
-    var imagePreview = document.querySelector('.filter-image-preview');                 // изображение к которому применяется масштабирование
+  var initializeScale = function(scaleElement, adjustScale) {
+    var scaleField = scaleElement.querySelector('[type="text"]');
 
-    var MAX_SCALE_VALUE = 100;  // максимальное значение масштаба изображения
-    var MIN_SCALE_VALUE = 0;    // минимальное значение масштаба изображения
+    var DEFAULT_SCALE_VALUE = 50;
+    var MAX_SCALE_VALUE = 100;
+    var MIN_SCALE_VALUE = 0;
+    var STEP_SCALE_VALUE = 25;
 
-    // установим масштаб изображения в начаьное положение согласно defaultValue
-    imagePreview.style.transform = 'scale('+ defaultValue / 100 +')';
+    adjustScale(DEFAULT_SCALE_VALUE);
 
-    // навесим обработчи клика на кнопку увеличения масштаба
-    // и изменяем масштаб согласно установленным значениям
-
-    upScaleField.addEventListener('click', function() {
-      if (parseInt(scaleField.value) >= MAX_SCALE_VALUE) {
-        scaleField.value = MAX_SCALE_VALUE + '%';
-      } else {
-        scaleField.value = parseInt(scaleField.value) + step + '%';
+    scaleElement.addEventListener('click', function(event) {
+      if (event.target.innerHTML === '+' && parseInt(scaleField.value) < MAX_SCALE_VALUE) {
+        scaleField.value = parseInt(scaleField.value) + STEP_SCALE_VALUE + '%';
+        adjustScale(parseInt(scaleField.value));
+      } else if (event.target.innerHTML === '–' && parseInt(scaleField.value) > MIN_SCALE_VALUE) {
+        scaleField.value = parseInt(scaleField.value) - STEP_SCALE_VALUE + '%';
+        adjustScale(parseInt(scaleField.value));
       }
-    });
+    })
+  };
 
-    // навесим обработчи клика на кнопку уменьшения масштаба
-    // и изменяем масштаб согласно установленным значениям
-
-    downScaleField.addEventListener('click', function() {
-      if (parseInt(scaleField.value) <= MIN_SCALE_VALUE) {
-        scaleField.value = MIN_SCALE_VALUE + '%';
-      } else {
-        scaleField.value = parseInt(scaleField.value) - step + '%';
-      }
-    });
-
-    // ф-я addScaleClass() изменяет масштаб изображения в зависимости
-    // от установленных значений масштаба
-
-    function addScaleClass() {
-      imagePreview.style.transform = 'scale(' + parseInt(scaleField.value) / 100 + ')';
-    }
-
-    upScaleField.addEventListener('click', addScaleClass);
-    downScaleField.addEventListener('click', addScaleClass);
-  }
-
-  var scaleControls = document.querySelector('.upload-resize-controls');
-  var STEP_SCALE_VALUE = 25;        // шаг изменения масштаба изображения
-  var DEFAULT_SCALE_VALUE = 50;     // значение масштаба изображения по умолчанию
-
-  initializeScale(scaleControls, STEP_SCALE_VALUE, DEFAULT_SCALE_VALUE);
-
+  initializeScale(scaleElement, adjustScale);
 })();
